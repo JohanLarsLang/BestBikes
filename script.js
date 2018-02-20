d8a19a33f6380dfac36577446535d086e8dff1a6
Johan Lång <johan.lang@hotmail.com>
2018-02-20 15:44:18 +0100

    All articles added and all code validated

---
+++ script.js
@@ -0,0 +1,3 @@
+  document.mainForm1.onclick = function(){
+  var gender = document.querySelector('input[name = gender1]:checked').value;
+   result1.innerHTML = 'Is available in store now! Price: '+gender;}
